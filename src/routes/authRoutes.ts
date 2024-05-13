9f29da43f940b21c010a4747c812a9bfa832b0c5
alnnu <luannf1020@gmail.com>
2024-05-13 11:42:42 -0300

    .

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -8,7 +8,7 @@ const authController = require("../controllers/AuthController")
 
 const BaseRoute : String = "/api/v1/auth"
 
-routes.get(`${BaseRoute}/login`, loginUser, authController.login)
+routes.post(`${BaseRoute}/login`, loginUser, authController.login)
 
 routes.post(`${BaseRoute}/create`, createUser, authController.create)
 

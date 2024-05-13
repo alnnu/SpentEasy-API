c3b8f6912545b44d22a6415f269d4c390c1b4bd9
alnnu <luannf1020@gmail.com>
2024-05-13 11:42:02 -0300

    add: swagger

---
+++ src/routes/swaggerRoute.ts
@@ -0,0 +1,10 @@
+
+
+const router = require('express').Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger/docs.json');
+
+router.use('/docs', swaggerUi.serve);
+router.get('/docs', swaggerUi.setup(swaggerDocument));
+
+module.exports = router
